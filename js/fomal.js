cfa37fb78ca22a1a0ddbd709ece6c4d80a762fc9
GarthSiTu <GarthSiTu@outlook.com>
2025-04-01 23:24:19 +0800

    Site updated: 2025-04-01 23:24:19

--- js/fomal.js
+++ js/fomal.js
@@ -3177,7 +3177,7 @@ if (localStorage.getItem("blogbg") != undefined) {
     --default-bg: url(https://raw.githubusercontent.com/GarthSiTu/image-hosting/refs/heads/main/%E7%BD%91%E7%AB%99%E4%B8%BB%E9%A2%98%E5%BB%BA%E8%AE%BE/pc_day_background.jpg);
     --darkmode-bg:url(https://raw.githubusercontent.com/GarthSiTu/image-hosting/refs/heads/main/%E7%BD%91%E7%AB%99%E4%B8%BB%E9%A2%98%E5%BB%BA%E8%AE%BE/pc_night_background.jpg);
     --mobileday-bg: url(https://cdn.seovx.com/?mom=302);
-    --mobilenight-bg: url(https://raw.githubusercontent.com/GarthSiTu/image-hosting/refs/heads/main/%E7%BD%91%E7%AB%99%E4%B8%BB%E9%A2%98%E5%BB%BA%E8%AE%BE/hlnOFnight.png);
+    --mobilenight-bg: url(https://cdn.seovx.com/?mom=302);
   }`;
 }
 // 切换背景主函数
